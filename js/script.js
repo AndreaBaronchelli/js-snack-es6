075fd8d6bc74097f5585506ebde8e18bbe0ada7a
Andrea Baronchelli <baronchelli.andrea@gmail.com>
2021-04-15 18:01:57 +0200

    init repo

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// Snack 1
+// Creare un array di oggetti: 
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. 
+// Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
+
+const bikes = [
+    { 
+        name: "Graziella",
+        peso: 6,
+    },
+    { 
+        name: "Saltafoss",
+        peso: 8,
+    },
+    { 
+        name: "Bianchi",
+        peso: 5,
+    },
+];
+
+for(let i = 0; i < bikes.length; i++){
+    
+}
