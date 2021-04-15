55a82ed759bd5a823176107eff358452a6ae2fe1
Andrea Baronchelli <baronchelli.andrea@gmail.com>
2021-04-15 18:19:21 +0200

    snack 2

--- js/script.js
+++ js/script.js
@@ -27,3 +27,60 @@ for(let i = 0; i < bikes.length; i++){
 };
 
 console.log(lightestBike);
+
+// Snack 2
+// Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+// Nome sarà l'unica proprietà da compilare, le altre saranno tutte settate a 0.
+// Generare numeri random al posto degli 0 nelle proprietà:
+// Punti fatti e falli subiti
+// Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+const teams = [
+    {
+        name: "Milan",
+        points: 0,
+        fouls: 0,
+    },
+    {
+        name: "Atalanta",
+        points: 0,
+        fouls: 0,
+    },
+    {
+        name: "Torino",
+        points: 0,
+        fouls: 0,
+    },
+    {
+        name: "Chievo",
+        points: 0,
+        fouls: 0,
+    },
+];
+
+for(let i = 0; i < teams.length; i++){
+    teams[i].points = randNumber(1, 100);
+    teams[i].fouls = randNumber(1, 100);
+}
+
+console.log(teams);
+
+let newTeams = [];
+
+for(let i = 0; i < teams.length; i++){
+    newTeams.push(
+        {
+        name: teams[i].name,
+        fouls: teams[i].fouls
+        }
+    );
+};
+
+console.log(newTeams);
+/********************************
+* FUNCTIONS
+*********************************/
+
+function randNumber(min, max) {
+    return Math.floor(Math.random()* (max - min) + 1) + min;
+}
